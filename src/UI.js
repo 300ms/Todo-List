c9af0024636043414e8639cbedaf8d450021586b
300ms <ilhansnmz44@gmail.com>
2020-11-27 01:45:42 +0300

    Add method to show todos of a specific project

--- src/UI.js
+++ src/UI.js
@@ -1,9 +1,9 @@
 import tasks from './todos';
 
 class UI {
-  static showProject(){
+  static listProjects() {
     const content = document.querySelector('.content');
-    const ul = document.querySelector('.project-ul');  
+    const ul = document.querySelector('.project-ul');
 
     tasks.forEach(task => {
       const li = `
@@ -11,14 +11,35 @@ class UI {
             <h4>${task.projectName}</h4>
           <span class="badge badge-primary badge-pill">14</span>
         </li>
-      `
+      `;
       ul.innerHTML += li;
-
-    })
+    });
 
     content.appendChild(ul);
   }
 
+  static showProject() {
+    const parent = document.querySelector('.todos');
+    const proj = tasks[0];/* .forEach(project => {
+      if (project.projectName === title);
+      return project;
+    }); */
+
+    proj.projectTodos.forEach(todo => {
+      const div = `
+      <div class="card text-white bg-primary mb-3" style="max-width: 20rem;">
+        <div class="card-header">buttons</div>
+        <div class="cardBody">
+          <h4 class="card-title">${todo.title}</h4>
+          <p class="card-text">${todo.description}</p>
+        </div>
+        <div class="card-footer"><span>${todo.dueDate}</span><span>${todo.priority}</span></div>
+      </div>
+      `;
+
+      parent.innerHTML += div;
+    });
+  }
 }
 
-export { UI as default }
+export { UI as default };
--- src/index.js
+++ src/index.js
@@ -4,4 +4,5 @@ import 'bootswatch/dist/pulse/bootstrap.min.css';
 import UI from './UI';
 
 
-document.addEventListener('DOMContentLoaded', UI.showProject())
+document.addEventListener('DOMContentLoaded', UI.listProjects());
+document.addEventListener('DOMContentLoaded', UI.showProject());
