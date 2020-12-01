d20e691c926fb5a81c7a2cc1104eda7f85d51b40
300ms <ilhansnmz44@gmail.com>
2020-12-01 21:49:34 +0300

    Remove forgotten debugging functions

--- src/index.js
+++ src/index.js
@@ -2,11 +2,9 @@ import './style.css';
 import 'bootstrap';
 import 'bootswatch/dist/pulse/bootstrap.min.css';
 import UI from './UI';
-import tasks from './storage';
 
 document.addEventListener('DOMContentLoaded', UI.listProjects());
 document.addEventListener('DOMContentLoaded', UI.showProject('project one'));
-document.addEventListener('DOMContentLoaded', console.log(tasks));
 
 document.querySelector('.project-ul').addEventListener('click', (event) => {
   if (event.target.classList.contains('project-title')) {
@@ -55,6 +53,5 @@ document.querySelectorAll('.cancelProject').forEach(button => {
 document.querySelectorAll('.deleteProject').forEach(button => {
   button.addEventListener('click', e => {
     UI.deleteProject(e);
-    console.log(tasks);
   });
 });
